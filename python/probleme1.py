231254c8d393c725579cd34b53b9a3901d01067e
Nicolas Paradis <nicolasparadis42@gmail.com>
2016-10-03 18:24:46 -0400

    modifie probleme1.py

--- python/probleme1.py
+++ python/probleme1.py
@@ -1,7 +1,8 @@
-def divisor(x):
-	
-	multi3 = [y * 3 for y in range(1, int((x - 1)/3) + 1)]
-	
-	multi5 = [z * 5 for z in range(1, int((x - 1)/5) + 1)]
-	
-	return(sum(set(multi3 + multi5)))
+def multiList(x, y):
+	return [z * y for z in range(1, int((x - 1) / y) + 1)] 
+
+def main():
+	print(sum(set(multiList(1000, 3) + multiList(1000, 5))))
+
+if __name__ == "__main__":
+    main()
